5b531f9061a3b42724b48af9463faffb4b51b0b9
Fraser Dempster <fraserdempster23@gmail.com>
2024-02-20 19:37:59 +0000

    update: svg to use tailwind preventing console errors

--- src/renderer/AddButton/AddButton.tsx
+++ src/renderer/AddButton/AddButton.tsx
@@ -5,17 +5,12 @@ export default function AddButton() {
         <button className="bg-[#4CAF50] hover:bg-green-800 text-white py-4 px-4 rounded-full focus:outline-none focus:shadow-outline flex items-center justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-10 w-10"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 4v16m8-8H4"
-            />
+            <path className="stroke-2" d="M12 4v16m8-8H4" />
           </svg>
         </button>
       </div>
